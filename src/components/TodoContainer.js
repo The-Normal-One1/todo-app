fd7f2802b1850e96f674f83072de05013ea155e8
Ermi <manermidem@gmail.com>
2022-12-19 17:24:08 +0300

    Edit files

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TodosList from './TodosList';
 import Header from './Header';
+import InputTodo from './InputTodo';
 
 class TodoContainer extends React.Component {
   state = {
@@ -22,11 +23,57 @@ class TodoContainer extends React.Component {
       },
     ],
   };
+
+  handleChange = (id) => {
+    // console.log('You CLICKED', id);
+    // this.setState((prevState) => {
+    //     return {
+    //       todos: prevState.todos.map((todo) => {
+    //         if (todo.id === id) {
+    //           return {
+    //             ...todo,
+    //             completed: !todo.completed,
+    //           };
+    //         }
+    //         return todo;
+    //       }),
+    //     };
+    //   }); or removing the () and put return instead
+
+    this.setState((prevState) => ({
+      todos: prevState.todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      }),
+    }));
+  };
+
+  delTodo = (id) => {
+    // console.log('DELETED', id);
+    this.setState({
+      todos: [
+        ...this.state.todos.filter((todo) => {
+          return todo.id !== id;
+        }),
+      ],
+    });
+  };
+
   render() {
     return (
       <>
         <Header />
-        <TodosList todos={this.state.todos} />
+        <InputTodo />
+        <TodosList
+          todos={this.state.todos}
+          handleChangeProps={this.handleChange}
+          delTodoProps={this.delTodo}
+        />
       </>
     );
   }
