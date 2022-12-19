1046ee4a11da62af1d5e67f8e45f3be3303a7214
Ermi <manermidem@gmail.com>
2022-12-19 15:13:31 +0300

    Create components and edit them for the app

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// function Header(props) or
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Todo Lists</h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/TodoItem.js
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+class TodoItem extends Component {
+  render() {
+    return <li>{this.props.todo.title}</li>;
+  }
+}
+
+export default TodoItem;
