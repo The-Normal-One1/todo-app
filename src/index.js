2beae01d00c4196c1168cf56ea8f9cf8e5fe9659
Ermi <manermidem@gmail.com>
2022-12-20 15:29:34 +0300

    Edit the index.js file

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import TodoContainer from './functionBased/components/TodoContainer';
+import { BrowserRouter } from 'react-router-dom';
 // stylesheet
 import './functionBased/index.css';
-// import App from './App';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-// const element = <h1> Hello from React World I am from Ethiopia </h1>;
 const value = ReactDOM.createRoot(document.getElementById('root'));
 value.render(
   <React.StrictMode>
-    <TodoContainer />
+    <BrowserRouter>
+      <TodoContainer />
+    </BrowserRouter>
   </React.StrictMode>
 );
