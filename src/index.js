0870d640da75b4530217590c3a292416c1622685
Ermi <manermidem@gmail.com>
2022-12-20 09:22:22 +0300

    Import sylesheet for the app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import TodoContainer from './components/TodoContainer';
-
-// import './index.css';
+// stylesheet
+import './index.css';
 // import App from './App';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
